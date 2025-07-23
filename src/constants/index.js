0079665ca2c6970c2721490a7814d883bbaf6f60
Prince <princespno21332@gmail.com>
2025-07-23 15:34:00 +0530

    final Commit

---
+++ src/constants/index.js
@@ -0,0 +1,179 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture, optimized databases, and seamless integrations—ensuring reliability at every layer.",
+    items: [
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Microservices, Auth Systems)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+    title: "DevOps Solutions",
+    description:
+      "Deploying software shouldn't be a gamble. I automate infrastructure, enforce security, and leverage cloud platforms (AWS) to keep your app running smoothly—24/7, at any scale.",
+    items: [
+      {
+        title: "CI/CD Pipelines",
+        description: "(GitHub Actions, Docker, Kubernetes)",
+      },
+      {
+        title: "Web App Management ",
+        description: "(Linux, Nginx, Load Balancing)",
+      },
+      {
+        title: "Performance Tuning",
+        description: "(Caching, Compression, Lighthouse 90+ Scores)",
+      },
+    ],
+  },
+  // {
+  //   title: "Security & Optimization",
+  //   description:
+  //     "Slow or hacked apps destroy trust. I harden security (XSS/SQLI protection, OAuth) and optimize bottlenecks so your app stays fast, safe, and scalable as you grow.",
+  //   items: [
+  //     {
+  //       title: "Code Audits",
+  //       description: "(Refactoring, Tech Debt Cleanup)",
+  //     },
+  //     {
+  //       title: "Pen Testing",
+  //       description: "(Vulnerability Assessments)",
+  //     },
+  //     {
+  //       title: "SEO Tech Stack",
+  //       description: "(SSR, Metadata, Structured Data)",
+  //     },
+  //   ],
+  // },
+  {
+    title: "Web Apps",
+    description:
+      "A clunky interface can sink even the best ideas. I craft responsive, pixel perfect web apps (NextJs , ReactJs , Gsap ) that users love—bridging design and functionality seamlessly and also provide an eye catching animations.",
+    items: [
+      {
+        title: "Top Notch UI",
+        description: "(Single codebase for iOS/Android/Web)",
+      },
+      {
+        title: "User Experience",
+        description: "(Offline mode, Push Notifications)",
+      },
+      {
+        title: "Performance",
+        description: "(Checkout flows, Payment Gateways, Inventory APIs)",
+      },
+    ],
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    name: "Trip It Today (Full Stack Hotel Booking and Listing Web App)",
+    description:
+      "A comprehensive platform for booking and listing hotels, offering a seamless user experience from search to reservation.",
+    href: "https://tripittoday.com/",
+    image: "/assets/projects/tripit.png",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Tailwind CSS" },
+      { id: 3, name: "Node.js" },
+      { id: 4, name: "MongoDB" },
+      { id: 5, name: "CI/CD setup" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Gamify Gaming Website",
+    description:
+      "A platform that offers a variety of games, allowing users to play, compete, and earn rewards.",
+    href: "https://gamify-theta.vercel.app/",
+    image: "/assets/projects/gamify.png",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "GSAP Animations" },
+      { id: 3, name: "CI/CD Setup" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  // {
+  //   id: 3,
+  //   name: "Apple Tech Marketplace",
+  //   description:
+  //     "An e-commerce platform for Apple products and accessories with deals and category filtering.",
+  //   href: "",
+  //   image: "/assets/projects/apple-tech-store.jpg",
+  //   bgImage: "/assets/backgrounds/map.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "Blazor" },
+  //     { id: 2, name: "ASP.NET Core" },
+  //     { id: 3, name: "SQL Server" },
+  //     { id: 4, name: "Bootstrap" },
+  //   ],
+  // },
+  {
+    id: 4,
+    name: "24 Global Solution",
+    description: "A Service based company's website for customer acquisition.",
+    href: "https://www.24globalsolution.com/",
+    image: "/assets/projects/24gs.png",
+    bgImage: "/assets/backgrounds/poster.jpg",
+    frameworks: [
+      { id: 1, name: "Next.js" },
+      { id: 2, name: "MongoDB" },
+      { id: 3, name: "API integration" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Farmisha ",
+    description:
+      "A web app for farmers and hobby gardeners to detect and cure plant diseases and get information about hydroponics crops.",
+    href: "https://farmishaxgtm.vercel.app/",
+    image: "/assets/projects/farmisha.png",
+    bgImage: "/assets/backgrounds/table.jpg",
+    frameworks: [
+      { id: 1, name: "Next.js" },
+      { id: 2, name: "MongoDB" },
+      { id: 3, name: "Weather API" },
+      { id: 4, name: "Material UI" },
+    ],
+  },
+  // {
+  //   id: 6,
+  //   name: "Digital Game Store",
+  //   description:
+  //     "A gaming platform featuring discounted titles, top sellers, and genre-based browsing.",
+  //   href: "",
+  //   image: "/assets/projects/game-store.jpg",
+  //   bgImage: "/assets/backgrounds/curtains.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "Svelte" },
+  //     { id: 2, name: "Node.js" },
+  //     { id: 3, name: "MongoDB" },
+  //     { id: 4, name: "Chakra UI" },
+  //   ],
+  // },
+];
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/princesoni__777" },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/prince-soni-97163522b/",
+  },
+  { name: "GitHub", href: "https://github.com/PrinceSoni23" },
+];
---
+++ src/sections/ContactSummary.jsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+import Marquee from "../components/Marquee";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const ContactSummary = () => {
+  const containerRef = useRef(null);
+  const items = [
+    "Innovation",
+    "Precision",
+    "Trust",
+    "Collaboration",
+    "Excellence",
+  ];
+
+  useGSAP(() => {
+    gsap.to(containerRef.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+        markers: false,
+      },
+    });
+  }, []);
+  return (
+    <section
+      ref={containerRef}
+      className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16"
+    >
+      <div className="overflow-hidden font-light text-center contact-text-responsive">
+        <p>
+          “ Let’s build a <br />
+          <span className="font-normal">memorable</span> &{" "}
+          <span className="italic">inspiring</span> <br />
+          web application <span className="text-gold">together</span> “
+        </p>
+      </div>
+      <Marquee items={items} />
+    </section>
+  );
+};
+
+export default ContactSummary;
